717f8e340383a3f0721fba6c127e2063a9167010
Yuliia Mustafina <mystafinayulija@gmail.com>
2022-12-11 00:41:45 +0100

    add toys style

---
+++ src/pages/toys_page/NoToysMessage/NoToysMessage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function NoToysMessage() {
+  return (
+    <div className="no-toys">
+      <div className="no-toys__inner">
+        <h2 className="no-toys__text">Sorry, no match found</h2>
+      </div>
+    </div>
+  );
+}
